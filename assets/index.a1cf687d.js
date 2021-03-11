12467f9a12fae6763d41ed5de9f701cd43aa0943
longer008 <937723369@qq.com>
2021-03-11 08:02:16 +0000

    Update Vite-Vue 3.x-TypeScript-Element Plus-Starter Template 685b16d2d3929ed087a70a3f026386c2f6d523d2

--- assets/index.a1cf687d.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./axios.2f74e94d.js";const e=e=>t({url:"https://www.zhihu.com/api/v3/feed/topstory/hot-lists/total",method:"get",params:e}),o=e=>t({url:"https://m.weibo.cn/api/container/getIndex?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot",method:"get",params:e});export{o as a,e as g};
